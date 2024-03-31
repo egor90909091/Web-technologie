ffb20e70ee3bb7e5207ce86eea0bf853811b1756
egor90909091 <113285800+egor90909091@users.noreply.github.com>
2024-03-31 20:42:14 +0500

    электро карс гибриды

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleContent(element) {
+  var content = element.nextElementSibling.querySelector('p');
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
