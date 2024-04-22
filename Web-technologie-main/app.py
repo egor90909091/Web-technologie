cb81f53fd7a0327af98e436fb32dcc3b8fde7f8d
egor90909091 <113285800+egor90909091@users.noreply.github.com>
2024-04-22 13:51:08 +0500

    nav python

--- Web-technologie-main/app.py
+++ Web-technologie-main/app.py
@@ -1,19 +1,33 @@
-from flask import Flask, render_template, redirect, url_for, request
-
-# Flask - библиотека для запуска нашего приложения Flask - app
-# render_template - нужен для то чтобы ваша страница html отобразилась корреткно
-# redirect - нам понадобится для обработки запросы формы где мы перенаприм пользователя на страницу админ панели
-# url_for - вспомогательна библиотека для того чтобы сделать правильный переход по ссылке в нашем случеш мы будем ссылаться на adm_panel
-# request - обработчик запросов GET/POST и дргуих
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-#  наша корневая страиницу лендинда
+#  наша корневая страиницу лендинга
 @app.route("/")
 def home():
     # Загрузка и отображение главной страницы (landing page)
-    return render_template("index.html")
+    return render_template(
+        "index.html", linkabout="/about", linkservice="/service", linkcontact="/contact"
+    )
+
+
+@app.route("/about")
+def about():
+    # Загрузка и отображение страницы "О нас" (about page)
+    return render_template("about.html")
+
+
+@app.route("/service")
+def service():
+    # Загрузка и отображение страницы "Услуги" (service page)
+    return render_template("service.html")
+
+
+@app.route("/contact")
+def contact():
+    # Загрузка и отображение страницы "Контакты" (contact page)
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
